15ea14f93038c2d3530644539fcd4d46e8e202f6
Mohan-Raj-M <mohanraj44812@gmail.com>
2020-07-01 18:39:01 +0530

    made changes in index

--- src/index.js
+++ src/index.js
@@ -12,18 +12,6 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(authRoutes);
 
-// const mongoUri =
-//   "mongodb+srv://mohan:kougW0DWyp7Icpi5@cluster0-frvyr.mongodb.net/test?retryWrites=true&w=majority";
-
-// mongoose.connect(mongoUri, {
-//   useNewUrlParser: true,
-//   useCreateIndex: true,
-//   useUnifiedTopology: true,
-// });
-
-// mongoose.connection.on("connected", () => {
-//   console.log("Connected to mongo yeah");
-// });
 
 app.get("/",requireAuth, (req, res) => {
   res.send(`Your email:${req.user.email}`);
