a62f9b63406889c3551363ead76fca8d3d28be19
unknown <sandheep17102@gmail.com>
2020-07-30 20:20:21 +0530

    Rectified error

--- src/emails/accounts.js
+++ src/emails/accounts.js
@@ -1,10 +1,12 @@
 const sgMail = require('@sendgrid/mail')
+const jwt = require('jsonwebtoken')
 
 const sendgridApiKey = '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'
 
 sgMail.setApiKey(sendgridApiKey)
 
 const sendWelcomeEmail = (email, name) => {
+    const token = jwt.sign({email}, "Email_auth_key")
     sgMail.send({
         to: email,
         from: {
@@ -34,7 +36,7 @@ const sendWelcomeEmail = (email, name) => {
                     <h4>Welcome to the healthy society of Desika foods where only 100% orgainc products are sold to you</h4>
                     <h4>For security reasons, you need to verify your email before you can start your purchase.</h4>
                     <h4>Click the button below to verify your email:</h4>
-                    <a href="https://desikafood.herokuapp.com/verification/${email}" class="button">Verify</a>
+                    <a href="https://desikafood.herokuapp.com/verification/${token}" class="button">Verify</a>
                     <h4>If this email wasn't meant for you click the one below to unsubscribe</h4>
                     <a href="#" class="button">Unsubscribe</a>
                 </body>
--- src/routes/emailRoutes.js
+++ src/routes/emailRoutes.js
@@ -1,12 +1,19 @@
 const express = require("express")
 const router = express.Router()
+const jwt = require('jsonwebtoken')
 const User = require('../models/User')
 
-router.get('/verification/:email', async (req, res) => {
+router.get('/verification/:token', async (req, res) => {
     try {
-        const user = await User.findOne({email: req.params.email})
-        user['valid'] = true
-        res.send({email:req.params.email})
+        jwt.verify(req.params.token, "Email_auth_key", async (e, token) => {
+            if (e) {
+                return res.status(400).send({error: 'Invalid token'})
+            }
+
+            const {email} = token
+            await User.findOneAndUpdate({email}, {valid: true})
+            res.send({success: 'Email verified!'})
+        })
     } catch (e) {
         res.status(400).send(e.message)
     }
