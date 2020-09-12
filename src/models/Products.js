dd486caf840b2f16c3c2909ebd007fbd57b688d0
Mohan-Raj-M <mohanraj44812@gmail.com>
2020-09-12 19:17:15 +0530

    added img in schema

--- src/models/Products.js
+++ src/models/Products.js
@@ -1,35 +1,38 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    id : {
-        type : String,
-        trim : true,
-        required : true
-    },
-    description : {
-        type : String,
-        trim : true,
-        required : true
-    },
-    price : {
-        type : Number,
-        required : true,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Price cannot be negative')
-            }
-        }
-    },
-    rating: {
-        type: Number,
-        default: 5
-    },
-    category: {
-        type: String,
-        default: undefined
+  id: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Price cannot be negative');
+      }
     }
-})
+  },
+  rating: {
+    type: Number,
+    default: 5
+  },
+  category: {
+    type: String,
+    default: undefined
+  },
+  img: {
+    type: Buffer
+  }
+});
 
-const Product = mongoose.model('Product', productSchema)
+const Product = mongoose.model('Product', productSchema);
 
-module.exports = Product
+module.exports = Product;
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,60 +1,67 @@
-const express = require('express')
-const Product = require('../models/Products')
-const auth = require('../middleware/adminAuth')
-const sharp = require('sharp')
-const multer =require('multer')
-const { update } = require('../models/Products')
-const router = new express.Router()
+const express = require('express');
+const Product = require('../models/Products');
+const auth = require('../middleware/requireAuth');
+const sharp = require('sharp');
+const multer = require('multer');
+const { update } = require('../models/Products');
+const router = new express.Router();
 
 router.post('/product/uploadproducts', auth, async (req, res) => {
-    const product = new Product(req.body)
-    try {
-        await product.save()
-        res.status(200).send('product successfully sent!')
-    } catch (e) {
-        res.status(400).send(e.message)
-    }
-})
+  const product = new Product(req.body);
+  try {
+    await product.save();
+    res.status(200).send('product successfully sent!');
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+});
 
 const upload = multer({
-    limits : {
-        fileSize : 1000000
-    },
-    fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-            return cb(new Error('Please upload a photo!'))
-        }
-
-        cb(undefined, true)
+  limits: {
+    fileSize: 1000000
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error('Please upload a photo!'));
     }
-})
 
-router.post('/products/img/:id',auth,upload.single('img'), async (req, res) => {
-    const buffer = await sharp(req.file.buffer).png().toBuffer()
-    const product = await findOne({ id : req.params.id })
-    product.img = buffer
-    await product.save()
-    res.send()
-}, (error, req, res, next) => {
-    res.status(400).send({ error : error.message })
-})
+    cb(undefined, true);
+  }
+});
 
+router.post(
+  '/products/img/:id',
+  auth,
+  upload.single('img'),
+  async (req, res) => {
+    const buffer = await sharp(req.file.buffer).png().toBuffer();
+    const product = await findOne({ id: req.params.id });
+    product.img = buffer;
+    await product.save();
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
 
 router.patch('/products/editproduct/:id', auth, async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['description', 'price', 'img']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['description', 'price', 'img'];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
 
-    if (!isValidOperation) {
-        return res.status(400).send({ error : 'Invalid updates!!'})
-    }
-    try {
-        const product = await Product.findOne({id : req.params.id})
-        update.updates.forEach(update => product[update] = req.body[update])
-        res.send()
-    } catch (e) {
-        res.status(400).send(e.message)
-    }
-})
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates!!' });
+  }
+  try {
+    const product = await Product.findOne({ id: req.params.id });
+    update.updates.forEach((update) => (product[update] = req.body[update]));
+    res.send();
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+});
 
 module.exports = router;
